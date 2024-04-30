6918906d8b91e525a8cb811a6b8f527730064943
yawar1101 <mohdyawars@gmail.com>
2024-04-30 23:00:42 +0530

    Added home.html

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+BASE_DIR = pathlib.Path(__file__).parent
 
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
 
 # HTTP GET
-@app.get("/")
-def home_view():
-    return {"hello": "world"}
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
+
 
 # HTTP POST
 @app.post("/")
